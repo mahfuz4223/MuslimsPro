c5ce98f71492dc468da378dae2939095a65b6517
Mahfuz <mahfuz4223@gmail.com>
2023-10-09 15:06:11 +0600

    add bangla tools

---
+++ app/src/main/java/com/dark/muslimspro/tools/BanglaDateConverter.java
@@ -0,0 +1,257 @@
+package com.dark.muslimspro.tools;
+
+import com.dark.muslimspro.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class BanglaDateConverter {
+
+    public static String convertToBanglaNumber(String input) {
+        String[] numbers = {"০", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯"};
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch >= '0' && ch <= '9') {
+                result.append(numbers[ch - '0']);
+            } else {
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String pickBanglaDate() {
+
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH) + 1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("MMMM");
+        String strMonth = formatter.format(date);
+
+        String Month = "", banglaNumber = "";
+        int banglaDay = 1, i, dayNumber = 1, banglaYear;
+        banglaYear = year - 593;
+
+        if (strMonth.equals("April") && day <= 13) {
+            Month = "চৈত্র"; //চৈত্র
+            banglaYear = banglaYear - 1;
+            dayNumber = 1;
+            banglaDay = 14;
+            for (i = dayNumber; i > day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("April") && day > 13) {
+            Month = "বৈশাখ"; //বৈশাখ
+            dayNumber = 14;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("May") && day <= 14) {
+            Month = "বৈশাখ"; //বৈশাখ
+            dayNumber = 1;
+            banglaDay = 15;
+            for (i = dayNumber; i > day; i++) {
+                banglaDay = banglaDay + 1;
+
+            }
+        } else if (strMonth.equals("May") && day > 14) {
+            Month = "জৈষ্ঠ্য"; //জৈষ্ঠ্য
+            dayNumber = 15;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("June") && day <= 14) {
+            Month = "জৈষ্ঠ্য"; //জৈষ্ঠ্য
+            dayNumber = 1;
+            banglaDay = 15;
+            for (i = dayNumber; i > day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("June") && day > 14) {
+            Month = "আষাঢ়"; //আষাঢ়
+            dayNumber = 15;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("July") && day <= 15) {
+
+            Month = "আষাঢ়"; //আষাঢ়
+            dayNumber = 1;
+            banglaDay = 16;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + day;
+            }
+
+        } else if (strMonth.equals("July") && day > 15) {
+            Month = "শ্রাবণ"; //শ্রাবণ
+            dayNumber = 16;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("August") && day <= 15) {
+            dayNumber = 1;
+            banglaDay = 16;
+            Month = "শ্রাবণ"; //শ্রাবণ
+
+            for (i = dayNumber; i <= day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("August") && day > 15) {
+            dayNumber = 16;
+            banglaDay = 1;
+            Month = "ভাদ্র"; //ভাদ্র
+
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("September") && day <= 15) {
+            Month = "ভাদ্র"; //ভাদ্র
+            dayNumber = 1;
+            banglaDay = 16;
+
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+
+            }
+        } else if (strMonth.equals("September") && day > 15) {
+            Month = "আশ্বিন"; //আশ্বিন
+            dayNumber = 16;
+            banglaDay = 1;
+
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+
+            }
+        } else if (strMonth.equals("October") && day <= 16) {
+            Month = "আশ্বিন"; //আশ্বিন
+
+            dayNumber = 1;
+            banglaDay = 17;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+
+            }
+        } else if (strMonth.equals("October") && day > 16) {
+            Month = "কার্ত্তিক"; //কার্ত্তিক
+            dayNumber = 17;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("November") && day <= 15) {
+            Month = "কার্ত্তিক"; //কার্ত্তিক
+            dayNumber = 1;
+            banglaDay = 16;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+
+            }
+
+        } else if (strMonth.equals("November") && day > 15) {
+            Month = "অগ্রহায়ণ"; //অগ্রহায়ণ
+            dayNumber = 16;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("December") && day <= 15) {
+
+            Month = "অগ্রহায়ণ"; //অগ্রহায়ণ
+            dayNumber = 1;
+            banglaDay = 16;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("December") && day > 15) {
+            Month = "পৌষ"; //পৌষ
+            dayNumber = 16;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("January") && day <= 14) {
+            Month = "পৌষ"; //পৌষ
+            banglaYear = banglaYear - 1;
+            dayNumber = 1;
+            banglaDay = 17; //for 15
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+
+            }
+        } else if (strMonth.equals("January") && day > 14) {
+            Month = "মাঘ"; //মাঘ
+            banglaYear = banglaYear - 1;
+            dayNumber = 15;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("February") && day <= 13) {
+            Month = "মাঘ"; //মাঘ
+            banglaYear = banglaYear - 1;
+            dayNumber = 1;
+            banglaDay = 14;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+
+            }
+        } else if (strMonth.equals("February") && day > 13) {
+            Month = "ফাল্গুন"; //ফাল্গুন
+            banglaYear = banglaYear - 1;
+            dayNumber = 14;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        } else if (strMonth.equals("March") && day <= 14) {
+            Month = "ফাল্গুন"; //ফাল্গুন
+            banglaYear = banglaYear - 1;
+            dayNumber = 1;
+            banglaDay = 15;
+
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+                if (((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 00))) {
+                    if (banglaDay > 30) {
+                        banglaDay = 1;
+                        break;
+                    }
+                } else {
+                    if (banglaDay > 29) {
+                        banglaDay = 1;
+                        break;
+                    }
+                }
+
+            }
+
+        } else if (strMonth.equals("March") && day > 14) {
+            Month = "চৈত্র;"; //চৈত্র
+            banglaYear = banglaYear - 1;
+            dayNumber = 15;
+            banglaDay = 1;
+            for (i = dayNumber; i < day; i++) {
+                banglaDay = banglaDay + 1;
+            }
+        }
+
+        return ((convertToBanglaNumber(String.valueOf(banglaDay - 1))) + " " + Month + " " + (convertToBanglaNumber(String.valueOf(banglaYear))));
+    }
+
+    public void main(String[] args) {
+        String convertedNumber = convertToBanglaNumber("12345");
+        System.out.println("Converted Number: " + convertedNumber);
+
+        String banglaDate = pickBanglaDate();
+        System.out.println("Bangla Date: " + banglaDate);
+    }
+}
